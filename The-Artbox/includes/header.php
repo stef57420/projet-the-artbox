c0cedd89f20fa26ea4c3139891eb1f0d584e5bc7
Stéphnaie PERRIN <159540656+stef57420@users.noreply.github.com>
2024-02-12 11:42:55 +0100

    Delete The-Artbox directory

--- The-Artbox/includes/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <a href="index.php"><img src="img/logo.png" alt="Logo Artbox" id="logo" /></a>
-    <nav>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-        </ul>
-    </nav>
-</header>
